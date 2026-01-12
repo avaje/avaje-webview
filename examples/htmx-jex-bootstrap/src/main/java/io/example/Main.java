89db1e88fcfbccde1cf714525ae9f6efcc4cd7a6
robin.bygrave <robin.bygrave@eroad.com>
2026-01-13 10:58:58 +1300

    [example] Tidy htmx-jex-bootstrap example

--- examples/htmx-jex-bootstrap/src/main/java/io/example/Main.java
+++ examples/htmx-jex-bootstrap/src/main/java/io/example/Main.java
@@ -4,7 +4,12 @@ import io.avaje.inject.*;
 import io.avaje.jex.Jex;
 import io.avaje.webview.Webview;
 
+import static java.lang.System.Logger.Level.DEBUG;
+
 public class Main {
+
+    private static final System.Logger log = System.getLogger("app");
+
     static void main(String[] args) {
 
         var server = Jex.create()
@@ -13,23 +18,20 @@ public class Main {
                 .start();
 
         int port = server.port();
+        String url = "http://localhost:" + port;
 
-        Webview wv = Webview.builder()
+        try (Webview wv = Webview.builder()
                 .enableDeveloperTools(true)
                 .extractToUserHome(true)
                 .title("My App")
                 .width(1000)
                 .height(800)
-                .url("http://localhost:" + port)
-                .build();
+                .url(url)
+                .build()) {
 
-        // jex.lifecycle().onShutdown(() -> System.out.println("lifecycle stop"));
-        // server.onShutdown(wv::close); //, Integer.MIN_VALUE);
-
-        wv.run(); // Run the webview event loop, the webview is fully disposed when this returns.
-        System.out.println("normal stop");
-        // wv.close(); // Free any resources allocated.
-        server.shutdown();
-        System.out.println("done");
+            wv.run(); // Run the webview event loop, the webview is fully disposed when this returns.
+            server.shutdown();
+            log.log(DEBUG, "done");
+        }
     }
 }
