55004bd1683bde1c9a7fdb0e88212a274647f62f
robin.bygrave <robin.bygrave@eroad.com>
2026-01-20 16:29:04 +1300

    Fix avaje-webview-platform, it can't find the resource modules

--- avaje-webview-platform/src/main/java/module-info.java
+++ avaje-webview-platform/src/main/java/module-info.java
@@ -4,8 +4,8 @@
  */
 module io.avaje.webview.platform {
   requires transitive io.avaje.webview;
-  requires static io.avaje.webview.linux32;
-  requires static io.avaje.webview.linux64;
-  requires static io.avaje.webview.windows;
+  // requires static io.avaje.webview.linux32;
+  // requires static io.avaje.webview.linux64;
+  // requires static io.avaje.webview.windows;
   // requires static io.avaje.webview.mac;
 }
