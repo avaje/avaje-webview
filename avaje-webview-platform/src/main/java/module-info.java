2a4ea00c480531fb47983f16bbef495696738ed7
Rob Bygrave <robin.bygrave@gmail.com>
2026-01-20 19:05:34 +1300

    Add avaje-webview-platform with maven profiles for conditional depend… (#66)
    * Add avaje-webview-platform with maven profiles for conditional dependencies
    
    In this way, dependencies are included based on the build os family. Can be used
    when using native image to build multiple platform executables (and these would
    be optimised to only include the native libraries for their platform).
    
    * Fix avaje-webview-platform profile linux64
    
    * Fix avaje-webview-platform, it can't find the resource modules
    
    ---------
    
    Co-authored-by: robin.bygrave <robin.bygrave@eroad.com>
    Co-authored-by: Josiah Noel <32279667+SentryMan@users.noreply.github.com>

---
+++ avaje-webview-platform/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+
+/**
+ * Aggregator for all native libraries that are conditional dependencies.
+ */
+module io.avaje.webview.platform {
+  requires transitive io.avaje.webview;
+  // requires static io.avaje.webview.linux32;
+  // requires static io.avaje.webview.linux64;
+  // requires static io.avaje.webview.windows;
+  // requires static io.avaje.webview.mac;
+}
