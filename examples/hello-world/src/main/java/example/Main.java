1f596374a203c07a9b114c14320f3ef91a61c315
Mechite <contact@mechite.com>
2026-01-23 04:18:23 +0000

    Wrap `examples/hello-world` builder to multiple lines
    Signed-off-by: Mahied Maruf <contact@mechite.com>

--- examples/hello-world/src/main/java/example/Main.java
+++ examples/hello-world/src/main/java/example/Main.java
@@ -5,7 +5,11 @@ import io.avaje.webview.Webview;
 public class Main {
 
   static void main() {
-    Webview webview = Webview.builder().title("Hi").html("<h1>Hello World!</h1>").enableDeveloperTools(true).build();
+    Webview webview = Webview.builder()
+        .title("Hi")
+        .html("<h1>Hello World!</h1>")
+        .enableDeveloperTools(true)
+        .build();
 
     // needs JVM argument -XstartOnFirstThread on Macos
     webview.run();
