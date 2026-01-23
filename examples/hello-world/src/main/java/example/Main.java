89fbe2ccfc7dc0456e564398e320daf8e03b0912
Mechite <contact@mechite.com>
2026-01-23 04:17:53 +0000

    Enable developer tools in `examples/hello-world`
    Signed-off-by: Mahied Maruf <contact@mechite.com>

--- examples/hello-world/src/main/java/example/Main.java
+++ examples/hello-world/src/main/java/example/Main.java
@@ -5,8 +5,7 @@ import io.avaje.webview.Webview;
 public class Main {
 
   static void main() {
-
-    Webview webview = Webview.builder().title("Hi").html("<h1>Hello World!</h1>").build();
+    Webview webview = Webview.builder().title("Hi").html("<h1>Hello World!</h1>").enableDeveloperTools(true).build();
 
     // needs JVM argument -XstartOnFirstThread on Macos
     webview.run();
